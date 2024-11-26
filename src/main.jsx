e4834dc880e47f06ef8d366663cfd397953e53ac
Andrey <viiixx@yandex.ru>
2024-11-26 12:32:22 +0300

    new chat feature

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker
-      .register("./serviceWorker")
-      .then((res) => console.log("service worker registered"))
-      .catch((err) => console.log("service worker not registered", err));
-  });
-}
+// if ("serviceWorker" in navigator) {
+//   window.addEventListener("load", function () {
+//     navigator.serviceWorker
+//       .register("./serviceWorker")
+//       .then((res) => console.log("service worker registered"))
+//       .catch((err) => console.log("service worker not registered", err));
+//   });
+// }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
