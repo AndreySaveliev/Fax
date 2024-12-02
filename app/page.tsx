08ebffc43f266c098bf5857a81c2b714859f4793
Andrey <viiixx@yandex.ru>
2024-12-02 15:38:25 +0300

    home page refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
-import { handleSendMessage } from "@/actions/actions";
+import { fetchUserChats, handleLogOut, handleSendMessage } from "@/actions/actions";
 import Input from "@/components/Input";
 import SidebarEl from "@/components/SidebarEl";
-import { db } from "@/db";
-import { chatsTable } from "@/db/schema";
-import { desc, eq } from "drizzle-orm";
 
 export default async function Home() {
-  const chats = await db.query.chatsTable.findMany({
-    where: eq(chatsTable.userId, 1),
-    orderBy: desc(chatsTable.lastMessageTimeStamp),
-  });
+  const chats = await fetchUserChats();
   return (
-    <div className="flex flex-col py-4 text-2xl h-full">
+    <div className="flex flex-col pt-4 text-2xl h-full">
+      <button className="absolute bottom-6 left-6" onClick={handleLogOut}>
+        logout
+      </button>
       <h2 className="mx-auto">Select existing chat</h2>
       {chats && chats.map((chat) => <SidebarEl key={chat.id} chat={chat} />)}
-      <div className="mx-auto flex flex-col justify-end flex-1 gap-5">
+      <div className="mx-auto flex flex-col justify-end flex-1 gap-5 w-full">
         <h2 className="text-center">Or create new chat</h2>
         <Input handleSendMessage={handleSendMessage} id={null} />
       </div>
