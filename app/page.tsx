100cc0658616a87c11b6f601f35525a141839a52
Andrey <viiixx@yandex.ru>
2024-12-03 16:31:56 +0300

    llog out btn change

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
   const chats = await fetchUserChats();
   return (
     <div className="flex flex-col pt-4 text-2xl h-full">
-      <button className="absolute bottom-6 left-6" onClick={handleLogOut}>
-        logout
-      </button>
       <h2 className="mx-auto">Select existing chat</h2>
       {chats && chats.map((chat) => <SidebarEl key={chat.id} chat={chat} />)}
+      <button className="text-sm underline" onClick={handleLogOut}>
+        logout
+      </button>
       <div className="mx-auto flex flex-col justify-end flex-1 gap-5 w-full">
         <h2 className="text-center">Or create new chat</h2>
         <Input handleSendMessage={handleSendMessage} id={null} />
